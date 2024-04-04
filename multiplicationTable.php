13388dd59a328c3b8d62aef097dd5d0ec65ce698
Raj <rajkaura@ku.edu>
2024-04-04 17:07:16 -0500

    updated formatting of problem 1 and attempted problem 4 (need to test on lab computer)

---
+++ multiplicationTable.php
@@ -0,0 +1,21 @@
+<?php
+$size = $_POST["size"];
+if($size > 0) {
+
+    echo "<h2>Multiplication Table for $size</h2>";
+    echo "<table>";
+
+    for($i = 1; $i <= $size; $i++) {
+        echo "<tr>";
+        for($j = 1; $j <= $size; $j++) {
+            echo "<td>" . ($i * $j) . "</td>";
+
+        }
+        echo "</tr>";
+    }
+    echo "</table>";
+} else {
+    echo "<p>Enter a valid positive integer.</p>";
+}
+
+?>
